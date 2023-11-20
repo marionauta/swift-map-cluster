faaede67d32e6fca8535e462e32604a38b478541
Mario Nachbaur <marionauta@users.noreply.github.com>
2023-11-20 10:27:27 +0100

    refactor: separate view from `DefaultClusterContent`

--- Sources/MapCluster/Content/DefaultClusterContent.swift
+++ Sources/MapCluster/Content/DefaultClusterContent.swift
@@ -11,19 +11,32 @@ public struct DefaultClusterContent<Single: MapSingle>: MapContent {
 
     public var body: some MapContent {
         Annotation(coordinate: cluster.coordinate, anchor: .center) {
-            Text(String(cluster.singles.count))
-                .minimumScaleFactor(0.5)
-                .foregroundStyle(Color.white)
-                .padding(2)
-                .frame(width: 25, height: 25, alignment: .center)
-                .background(.tint)
-                .clipShape(Circle())
-                .padding(1.5)
-                .background(Color.white)
-                .clipShape(Circle())
-                .shadow(radius: 2, y: 2)
+            DefaultClusterView(count: cluster.singles.count)
         } label: {
             EmptyView()
         }
+    }
+}
+
+@available(iOS 15.0, *)
+public struct DefaultClusterView: View {
+    private let count: Int
+
+    public init(count: Int) {
+        self.count = count
+    }
+
+    public var body: some View {
+        Text(String(count))
+            .minimumScaleFactor(0.5)
+            .foregroundStyle(Color.white)
+            .padding(2)
+            .frame(width: 25, height: 25, alignment: .center)
+            .background(.tint)
+            .clipShape(Circle())
+            .padding(1.5)
+            .background(Color.white)
+            .clipShape(Circle())
+            .shadow(radius: 2, y: 2)
     }
 }
